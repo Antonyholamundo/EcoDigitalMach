1db77788f934273fbb017673f11862ec0b9d8887
Anthony Erreyes <antony23w@gmail.com>
2025-12-21 00:02:36 -0500

    Force HTTPS schema in production to fix insecure form warning

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Facades\URL;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -19,6 +21,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Forzar HTTPS en producción (Render)
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         // Fix para Neon DB en Windows (inyectar endpoint ID)
         $host = config('database.connections.pgsql.host');
         
